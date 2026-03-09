2722c1783bd883a41a8d1e999c04051a0e31aba8
SebastianT <sebastiantnalu@gmail.com>
2026-03-09 12:56:05 +0200

    Add security group for web access in Terraform

---
+++ terraform/security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "web_sg" {
+  name   = "sebastian"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
